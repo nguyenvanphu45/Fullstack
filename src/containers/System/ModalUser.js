b0af2d5118511aa70514e5125e8ac84ee1af0e03
Phu <vanphu040520022gmail.com>
2022-03-08 19:14:32 +0700

    CRUD with React (DELETE & use EMITTER to clear data)

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { emitter } from '../../utils/emitter';
+
 class ModalUser extends Component {
 
     constructor(props){
@@ -13,6 +15,21 @@ class ModalUser extends Component {
             lastName: '',
             address: '',
         }
+
+        this.listenToEmitter()
+    }
+
+    listenToEmitter() {
+        emitter.on('EVENT_CLEAR_MODAL_DATA', () => {
+            // reset state
+            this.setState({
+                email: '',
+                password: '',
+                firstName: '',
+                lastName: '',
+                address: '',
+            })
+        })
     }
 
     componentDidMount() {
