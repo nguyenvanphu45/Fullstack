00d54c55f4d6fd2f04012735995493b8ae369ab3
Phu <vanphu040520022gmail.com>
2022-03-15 15:01:15 +0700

    Complete the Authorization Menu for Admins (#55)

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -6,6 +6,7 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils/constant'
+import { FormattedMessage } from 'react-intl';
 
 class Header extends Component {
 
@@ -14,8 +15,7 @@ class Header extends Component {
     }
 
     render() {
-        const { processLogout, language } = this.props;
-
+        const { processLogout, language, userInfo } = this.props;
         return (
             <div className="header-container">
                 {/* thanh navigator */}
@@ -24,6 +24,10 @@ class Header extends Component {
                 </div>
 
                 <div className='languages'>
+                    <span className='welcome'>
+                        <FormattedMessage id='homeheader.welcome' />
+                        {userInfo && userInfo.firstName ? userInfo.firstName : ''} !
+                    </span>
                     <span className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
                     >
@@ -48,7 +52,8 @@ class Header extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language
+        language: state.app.language,
+        userInfo: state.user.userInfo,
     };
 };
 
