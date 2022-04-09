f6d7dd84d3fc00a001627e7fb51d82de903ed900
Phu <vanphu040520022gmail.com>
2022-04-09 11:02:02 +0700

    Design interface function appointment for medical (#72)

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -271,3 +271,26 @@ export const saveDetailDoctors = (data) => {
         }
     }
 }
+
+export const fetchAllScheduleTime = () => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await getAllCodeService("TIME")
+            if ( res && res.errCode === 0 ) {
+                dispatch({
+                    type: actionTypes.FETCH_ALL_SCHEDULE_TIME_SUCCESS,
+                    dataTime: res.data
+                })
+            } else {
+                dispatch({
+                    type: actionTypes.FETCH_ALL_SCHEDULE_TIME_FAILED,
+                })
+            }
+        } catch (e) {
+            console.log('FETCH_ALL_SCHEDULE_TIME_FAILED: ', e)
+            dispatch({
+                type: actionTypes.FETCH_ALL_SCHEDULE_TIME_FAILED,
+            })
+        }
+    }
+}
