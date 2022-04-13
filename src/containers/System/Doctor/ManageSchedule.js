d14f2d87e675939b58970b06d0154f9e7ae15140
Phu <vanphu040520022gmail.com>
2022-04-13 21:12:22 +0700

    Completing the Component Choose a Medical Examination Schedule (#77)

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -135,13 +135,19 @@ class ManageSchedule extends Component {
             doctorId: selectedDoctor.value,
             formattedDate: formattedDate
         })
-        console.log('check result: ', result)
-        console.log('check res: ', res)
+
+        if (res && res.errorCode === 0) {
+            toast.success('Save Infor succeed!')
+        } else {
+            toast.error("Error saveBulkScheduleDoctor !")
+            console.log('Error saveBulkScheduleDoctor >>> res: ', res)
+        }
     }
     
     render() {
         let { rangeTime } = this.state
         let { language } = this.props
+        let yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
 
         return (
             <div className='manage-schedule-container'>
@@ -164,7 +170,7 @@ class ManageSchedule extends Component {
                                 onChange={this.handleOnChangeDatePicker}
                                 className='form-control'
                                 value={this.state.currentDate}
-                                minDate={new Date()}
+                                minDate={yesterday}
                             />
                         </div>
                         <div className='col-12 pick-hour-container'>
