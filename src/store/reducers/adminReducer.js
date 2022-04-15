1a600a616027157c7afb641423e9a4b101e8d766
Phu <vanphu040520022gmail.com>
2022-04-15 21:33:12 +0700

    Add interface save doctor infor & Table doctor infor (#78)

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -9,6 +9,8 @@ const initialState = {
     topDoctors: [],
     allDoctors: [],
     allScheduleTime: [],
+
+    allRequiredDoctorInfor: [], 
 }
 
 const adminReducer = (state = initialState, action) => {
@@ -102,6 +104,18 @@ const adminReducer = (state = initialState, action) => {
 
         case actionTypes.FETCH_ALL_SCHEDULE_TIME_FAILED:
             state.allScheduleTime = []
+            return {
+                ...state
+            }
+
+        case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_SUCCESS:
+            state.allRequiredDoctorInfor = action.data
+            return {
+                ...state
+            }
+
+        case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_FAILED:
+            state.allRequiredDoctorInfor = []
             return {
                 ...state
             }
