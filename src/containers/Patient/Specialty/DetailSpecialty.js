68435bfd618b47a9c4f875eebbcf661704270a5a
Phu <vanphu040520022gmail.com>
2022-05-12 22:18:35 +0700

    Completing the Detailed Viewing Function of Medical Examination Specialty (#95)

--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -17,7 +17,8 @@ class DetailSpecialty extends Component {
         this.state = {
             arrDoctorId: [],
             dataDetailSpecialty: {},
-            listProvince: []
+            listProvince: [],
+            isShowDetailInfor: false
         }
     }
 
@@ -43,10 +44,22 @@ class DetailSpecialty extends Component {
                         })
                     }
                 }
+
+                let dataProvince = resProvince.data
+                if (dataProvince && dataProvince.length > 0) {
+                    dataProvince.unshift({
+                        createAt: null,
+                        keyMap: 'ALL',
+                        type: 'PROVINCE',
+                        valueEn: 'ALL',
+                        valueVi: 'Toàn quốc'
+                    })
+                }
+
                 this.setState({
                     dataDetailSpecialty: res.data,
                     arrDoctorId: arrDoctorId,
-                    listProvince: resProvince.data
+                    listProvince: dataProvince ? dataProvince : []
                 })
             }
         }
@@ -58,14 +71,45 @@ class DetailSpecialty extends Component {
         }
     }
 
-    handleOnChangeSelect = (event) => {
-        console.log('>>> check onChange: ', event.target.value)
+    handleOnChangeSelect = async (event) => {
+        if (this.props.match && this.props.match.params && this.props.match.params.id) {
+            let id = this.props.match.params.id
+            let location = event.target.value
+
+            let res = await getAllDetailSpecialtyById({
+                id: id,
+                location: location
+            })
+
+            if (res && res.errCode === 0) {
+                let data = res.data
+                let arrDoctorId = []
+                if (data && !_.isEmpty(res.data)) {
+                    let arr = data.doctorSpecialty
+                    if (arr && arr.length > 0) {
+                        arr.map(item => {
+                            arrDoctorId.push(item.doctorId)
+                        })
+                    }
+                }
+
+                this.setState({
+                    dataDetailSpecialty: res.data,
+                    arrDoctorId: arrDoctorId,
+                })
+            }
+        }
+    }
+
+    showHideDetailSpecialty = (status) => {
+        this.setState({
+            isShowDetailInfor: status
+        })
     }
 
     render() {
-        let {arrDoctorId, dataDetailSpecialty, listProvince} = this.state
+        let {arrDoctorId, dataDetailSpecialty, listProvince, isShowDetailInfor} = this.state
 
-        console.log('>>> check state: ', this.state)
         let { language } = this.props
         return (
             <div className='detail-specialty-container'>
@@ -73,7 +117,30 @@ class DetailSpecialty extends Component {
                 <div className='detail-specialty-body'>
                     <div className='description-specialty'>
                         {dataDetailSpecialty && !_.isEmpty(dataDetailSpecialty) &&
-                            <div dangerouslySetInnerHTML={{__html: dataDetailSpecialty.descriptionHTML}}>
+                            <div>
+                                {isShowDetailInfor === false && 
+                                    <>
+                                        <div dangerouslySetInnerHTML={{__html: dataDetailSpecialty.descriptionHTML}}
+                                            className='infor-specialty'
+                                        >
+                                        </div>
+
+                                        <span className='detail' onClick={() => this.showHideDetailSpecialty(true)}>
+                                            <FormattedMessage id="specialty.detail" />
+                                        </span> 
+                                    </>
+                                }
+
+                                {isShowDetailInfor === true && 
+                                    <>
+                                        <div dangerouslySetInnerHTML={{__html: dataDetailSpecialty.descriptionHTML}}>
+                                        </div>
+                                        <span className='detail' onClick={() => this.showHideDetailSpecialty(false)}>
+                                            <FormattedMessage id="specialty.hide" />
+                                        </span> 
+                                    </>
+                                }
+
 
                             </div>
                         }
@@ -100,6 +167,8 @@ class DetailSpecialty extends Component {
                                             <ProfileDoctor 
                                                 doctorId={item}
                                                 isShowDescriptionDoctor={true}
+                                                isShowLinkDetail={true}
+                                                isShowPrice={false}
                                                 // dataTime={dataTime}
                                             />
                                         </div>
