2e747e2388a26a756a60b9de3825d72841e2a675
Phu <vanphu040520022gmail.com>
2022-05-19 23:23:03 +0700

    Complete the function of managing doctor appointments (#100)

--- src/services/userService.js
+++ src/services/userService.js
@@ -97,6 +97,10 @@ const getAllPatientForDoctor = (data) => {
     return axios.get(`/api/get-list-patient-for-doctor?doctorId=${data.doctorId}&date=${data.date}`)
 }
 
+const postSendRemedy = (data) => {
+    return axios.post('/api/send-remedy', data)
+}
+
 export { 
     handleLoginApi, getAllUsers, 
     createNewUserService, deleteUserService, 
@@ -108,5 +112,5 @@ export {
     postPatientBookAppointment, postVerifyBookAppointment,
     createNewSpecialty, getAllSpecialty, getAllDetailSpecialtyById,
     createNewClinic, getAllClinic, getAllDetailClinicById,
-    getAllPatientForDoctor
+    getAllPatientForDoctor, postSendRemedy
 }
